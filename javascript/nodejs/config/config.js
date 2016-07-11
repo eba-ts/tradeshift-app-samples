a632cafb00dd6e3159beb28c225849b09783d80b
Artur_Kokhanskyi <artur.kokhanskyi@kindgeek.com>
2016-07-11 16:02:21 +0300

    Added Node.js API integration with Tradeshift

--- javascript/nodejs/config/config.js
+++
@@ -1,7 +0,0 @@
-/* All data comes from env variables */
-module.exports = {
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    authUrl: process.env.AUTH_URL,
-    tokenUrl: process.env.TOKEN_URL
-};
